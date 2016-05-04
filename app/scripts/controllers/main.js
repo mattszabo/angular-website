f24d72d8f184b48e5c7e268b92e60af442183fb7
Matt Szabo <szabo.matthew@gmail.com>
2016-05-04 21:59:49 +1000

    Revert "Make todo item an associative array and editable"

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,33 +11,22 @@ angular.module('mytodoApp')
   .controller('MainCtrl', function ($scope, localStorageService) {
 
     var todoListInStore = localStorageService.get('todoList');
-    $scope.todoList = todoListInStore || { text: '', isEditable: false };
+    $scope.todoList = todoListInStore || [];
+    $scope.isEditDisabled = true;
 
     $scope.$watch('todoList', function () {
       localStorageService.set('todoList', $scope.todoList);
     }, true);
 
-    $scope.addTodo = function(text) {
-      var todo = {todoText: text, isEditable: false};
-      if($scope.todoList.indexOf(text) === -1 && text.length > 0) {
-      console.log(todo);
+    $scope.addTodo = function(todo) {
+      if($scope.todoList.indexOf(todo) === -1 && todo.length > 0) {
           $scope.todoList.push(todo);
       }
-      $scope.newTodoText = "";
+      $scope.todo = "";
     };
 
     $scope.removeTodo = function(index) {
       $scope.todoList.splice(index,1);
-    };
-
-    $scope.editTodo = function(index, text){
-      //only save if we're done with editting and have clicked the edit button
-      console.log(index);
-      var todo = $scope.todoList[index];
-      if(todo.isEditable){
-        todo.text = text;
-      }
-      todo.isEditable = !todo.isEditable;
     };
 
   });
