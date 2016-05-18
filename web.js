faa893c08dd135ec7a3167158c58fd3d7a06f7ce
Matt szabo <wekilledit@users.noreply.github.com>
2016-05-18 11:23:56 +1000

    add http create server

--- web.js
+++ web.js
@@ -2,14 +2,13 @@ var gzippo = require('gzippo');
 var express = require('express');
 var morgan = require('morgan');
 var app = express();
+var http = require('http');
 
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 
-app.on('connect', function(){
+http.createServer(app).listen(process.env.PORT || 5000, function(){
   var host = server.address().address;
   var port = server.address().port;
   console.log("This resume app is listening at http://%s:%s and you should hire me :)", host, port);
-})
-
-app.listen(process.env.PORT || 5000);
+});
