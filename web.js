6a551cb1aef020628af1ade1d5104a9dfb2201d2
Matt szabo <wekilledit@users.noreply.github.com>
2016-05-18 11:31:23 +1000

    revert back to no http and logging in listen callback

--- web.js
+++ web.js
@@ -2,13 +2,12 @@ var gzippo = require('gzippo');
 var express = require('express');
 var morgan = require('morgan');
 var app = express();
-var http = require('http');
 
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 
-http.createServer(app).listen(process.env.PORT || 5000, function(){
+var server = app.listen(process.env.PORT || 5000, function(){
   var host = server.address().address;
   var port = server.address().port;
-  console.log("This resume app is listening at http://%s:%s and you should hire me :)", host, port);
+  console.log("This resume app is listening at http://%s:%s", host, port);
 });
